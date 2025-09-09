308311de4369c6fd6375502ed4acbd4f7be5048f
Jennifer <thejenniferhaggerty@gmail.com>
2025-09-08 20:12:56 -0400

    Nearly complete with carousel

--- scripts.js
+++ scripts.js
@@ -1,20 +1,28 @@
 let slideIndex = 1;
+let carousel = document.querySelector('.slides');
+let slides = document.getElementsByClassName("slide");
+//let slideWidth = slides[0].offsetWidth;
+
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  let cards = document.getElementsByClassName("card");
+  let cards = document.getElementsByClassName("slide.card");
+  const offset = 100 * slideIndex;
   if (n === 1) {
+    // user presses right arrow, content slides in from right
     for (i = 0; i < cards.length; i++) {
       let card = cards[i];
       card.classList.replace("slide-right", "slide-left");
     }
   } else {
+    // user presses left arrow, content slides in from left
     for (i = 0; i < cards.length; i++) {
       let card = cards[i];
       card.classList.replace("slide-left", "slide-right");
     }
-    }
+  }
+  carousel.style.transform = `translateX(${offset}%)`;
   showSlides(slideIndex += n);
 }
 
@@ -25,16 +33,18 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    const slide = slides[i]
+    slide.ariaHidden = true;
+    slide.ariaLive = false;
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].ariaHidden = false;
+  slides[slideIndex-1].ariaLive = true;
   dots[slideIndex-1].className += " active";
 }
