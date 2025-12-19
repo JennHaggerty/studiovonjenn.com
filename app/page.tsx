c84772aa1f5f900322e7a6834592bbfd424527bd
Jennifer <thejenniferhaggerty@gmail.com>
2025-12-18 22:52:07 -0500

    Clearer h1

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
               <h3>Summary</h3>
               <p>
                 Jennifer has been obsessed with light, color, and painting since
-                1997. She began photographying portraits in 2003. When she's not
+                1997. She began photographing portraits in 2003. When she's not
                 at the computer she's working on something creative or is out in
                 the garden with her partner and their dogs.
               </p>
