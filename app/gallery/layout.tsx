3c52c6be591162262d28b1da93f7a355c35e0967
Jennifer Haggerty <thejenniferhaggerty@protonmail.com>
2026-01-04 19:08:48 -0500

    Added atmos images from January

--- app/gallery/layout.tsx
+++ app/gallery/layout.tsx
@@ -2,9 +2,9 @@ import Footer from "../components/footer";
 
 export default function Layout(props: LayoutProps<"/">) {
   return (
-    <section className="flex flex-col justify-between h-screen max-w-[1200px]">
+    <div className="flex flex-col justify-between h-screen max-w-[1200px] m-auto">
       <div className="gallery-page">{props.children}</div>
       <Footer />
-    </section>
+    </div>
   );
 }
