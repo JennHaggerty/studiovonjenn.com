4302ed419f817366f84cf4ea1b52bee43f024b8a
Jennifer <thejenniferhaggerty@gmail.com>
2026-01-12 13:23:09 -0500

    rearrange homepage portfolio

--- app/components/HomepageGallery.tsx
+++ app/components/HomepageGallery.tsx
@@ -2,18 +2,6 @@ const HomepageGallery = () => {
   return (
     <div className="portfolio">
       <img
-        src="/images/jennifer-1.jpg"
-        alt="Jennifer wears a warm floral crown against her long teal hair, her dress and lips are a bold red"
-      />
-      <img
-        src="/images/jennifer-2.jpg"
-        alt="A lady must always smile is painted in blood above Jennifer's head in this Joker portrait"
-      />
-      <img
-        src="/images/jennifer-3.jpg"
-        alt="Jennifer is the burgundy Queen with an iron crown."
-      />
-      <img
         src="/images/birds-1.jpg"
         alt="A black raven mask with red eyes adorns Jennifer in this Raven in Flight portrait"
       />
@@ -24,6 +12,27 @@ const HomepageGallery = () => {
       <img
         src="/images/birds-3.jpg"
         alt="The Raven wearing a black cape and a black plague doctor mask with red eyes."
+      />
+      <img src="/images/safara-1.jpg" alt="Safara's portrait in a dark realm" />
+      <img
+        src="/images/sarah-2.jpg"
+        alt="A black spiral shell crown sits atop Sarah's head."
+      />
+      <img
+        src="/images/safara-2.jpg"
+        alt="Safara's portrait in a dark realm with lace."
+      />
+      <img
+        src="/images/jennifer-3.jpg"
+        alt="Jennifer is the burgundy Queen with an iron crown."
+      />
+      <img
+        src="/images/jennifer-2.jpg"
+        alt="A lady must always smile is painted in blood above Jennifer's head in this Joker portrait"
+      />
+      <img
+        src="/images/jennifer-1.jpg"
+        alt="Jennifer wears a warm floral crown against her long teal hair, her dress and lips are a bold red"
       />
       <img
         src="/images/winter-queen-1.jpg"
@@ -38,44 +47,23 @@ const HomepageGallery = () => {
         alt="Her crown and scepter of silver twigs, Melanie stands against a winter backdrop in falling snow"
       />
       <img
-        src="/images/tiffany-rae-knight-1.jpg"
-        alt="Victorian portrait of Tiffay in purple holding a mirror"
+        src="/images/tiffany-rae-knight-3.jpg"
+        alt="Portrait of Tiffay featured in Dark Beauty Magazine"
       />
       <img
         src="/images/tiffany-rae-knight-2.jpg"
         alt="Steampunk portrait of Tiffay in warm tones looking into your eyes"
       />
       <img
-        src="/images/tiffany-rae-knight-3.jpg"
-        alt="Portrait of Tiffay featured in Dark Beauty Magazine"
+        src="/images/tiffany-rae-knight-1.jpg"
+        alt="Victorian portrait of Tiffay in purple holding a mirror"
       />
-      <img
-        src="/images/lavender-dreams-1.jpg"
-        alt="A fantasy portrait of Jana in a whimsical purple forest"
-      />
-      <img
-        src="/images/lavendar-dreams-2.jpg"
-        alt="Jana sits against the dawn of the purple wood"
-      />
-      <img
-        src="/images/lavendar-dreams-3.jpg"
-        alt="A dream of Jana in lavender"
-      />
-      <img src="/images/paige-1.jpg" alt="Editorial portrait of Paige" />
+      <img src="/images/rozi-1.jpg" alt="Editorial portrait of Rozi" />
       <img
         src="/images/rozi-paige-1.jpg"
         alt="Editorial portrait of Paige and Rozi together"
       />
-      <img src="/images/rozi-1.jpg" alt="Editorial portrait of Rozi" />
-      <img src="/images/safara-1.jpg" alt="Safara's portrait in a dark realm" />
-      <img
-        src="/images/sarah-2.jpg"
-        alt="A black spiral shell crown sits atop Sarah's head."
-      />
-      <img
-        src="/images/safara-2.jpg"
-        alt="Safara's portrait in a dark realm with lace."
-      />
+      <img src="/images/paige-1.jpg" alt="Editorial portrait of Paige" />
       <img
         src="/images/demons-1.jpg"
         alt="Manzin wearing horns against a green background"
@@ -83,17 +71,14 @@ const HomepageGallery = () => {
       <img src="/images/demons-2.jpg" alt="A demon against a red background" />
       <img src="/images/demons-3.jpg" alt="Manzin as a demon biting a chain" />
       <img
-        src="/images/galleries/atmos/0126/atmos012610.jpg"
-        alt="Statuesque beauty in monochrome"
+        src="/images/egan-3.jpg"
+        alt="Egan wearing black horns, a red heart eye patch, and pentagram top for Gothesque Magazine"
       />
       <img
         src="/images/egan-2.jpg"
         alt="Egan is wearing ram horns and a white dress personifying Aries in this Gothesque editorial portrait"
       />
-      <img
-        src="/images/egan-3.jpg"
-        alt="Egan wearing black horns, a red heart eye patch, and pentagram top for Gothesque Magazine"
-      />
+      <img src="/images/egan-1.jpg" alt="Statuesque beauty" />
     </div>
   );
 };
