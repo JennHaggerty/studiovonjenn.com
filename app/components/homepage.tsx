ced976e4d935ed07e820c6228c88bd3bf49993df
Jennifer <thejenniferhaggerty@gmail.com>
2026-01-12 18:31:45 -0500

    bring in homepage

---
+++ app/components/homepage.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+import { galleries } from "../cms";
+import { GalleryInterface } from "../interfaces";
+import AdminDashboard from "./adminDashboard";
+
+export default function Homepage() {
+  const [activeGallery, setActiveGallery] = useState<GalleryInterface>();
+  const [showPreview, setShowPreview] = useState<boolean>(false);
+
+  const env = process.env.NODE_ENV;
+  const isAdmin = env !== "production";
+
+  const handleGalleryPageLinkClick = (directory: string) => {
+    if (!galleries) return;
+
+    const target = galleries.find((gallery) => gallery.directory === directory);
+    setActiveGallery(target);
+  };
+
+  const closeActiveGallery = () => {
+    setActiveGallery(undefined);
+  };
+
+  return (
+    <>
+      {isAdmin && (
+        <div className="sitewide-banner">
+          {showPreview ? (
+            <button onClick={() => setShowPreview(!showPreview)}>
+              Show admin dashboard
+            </button>
+          ) : (
+            <>
+              <button onClick={() => setShowPreview(!showPreview)}>
+                Show live view
+              </button>
+            </>
+          )}
+        </div>
+      )}
+      {isAdmin && !showPreview ? (
+        <AdminDashboard />
+      ) : activeGallery ? (
+        <GalleryPage
+          directory={activeGallery.directory}
+          title={activeGallery.title}
+          description={activeGallery.description}
+          onClose={closeActiveGallery}
+          onCloseText={activeGallery.onCloseText}
+        />
+      ) : (
+        <>
+          <HomepageHeader />
+          <HomepageGallery />
+          <div className="primary-bg">
+            <div className="col-2-img-right">
+              <div className="col col-text">
+                <About />
+                <PublicationsList />
+                <ExhibitList />
+                <EducationList />
+                <EventList onClick={handleGalleryPageLinkClick} />
+              </div>
+              <div className="col col-image">
+                <img
+                  src={`/images/ophelia-1.jpg`}
+                  alt="Ophelia in the waters"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
