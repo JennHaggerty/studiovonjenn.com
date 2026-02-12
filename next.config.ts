0047fea94207d0115be6512171542bc7af8a8ece
Jennifer <thejenniferhaggerty@gmail.com>
2026-02-12 17:07:35 -0500

    Redirects per Google analytics report

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/pdfs/Photography-Recipe-Book.pdf",
+        destination: "/prices",
+        permanent: true,
+      },
+      {
+        source: "/events/atmos/0925/index.html",
+        destination: "/gallery/atmos0925",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
